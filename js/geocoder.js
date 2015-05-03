e7cfd9e07eb9b76ec39bd0122c8b8bd8f0193827
Armaghan Behlum <armaghan23@gmail.com>
2015-05-03 19:13:29 -0400

    Working model

--- js/geocoder.js
+++ js/geocoder.js
@@ -1,37 +1,10 @@
 // Given click on country, launch simulation
 function clickCountry (lat, lng) {
-    country = getCountry(lat, lng);
-    
-    if (country == null) {
-        console.log("Could not start simulation");
-        return;
-    }
-
-    launchModel(country, lat, lng);
+    requestGeocode(lat, lng, launchModel, null);
 }
-
-function getCountry (lat, lng) {
-    var comp = "address_components" // Key to parse out country names
-    var geocode = requestGeocode(lat, lng); // Get country info
-
-    // Iterate through properties of result to find country
-    var country = null
-    for (var i = 0; i < geocode[comp].length; i++) {
-        if (geocode[comp][i].types[0] == "country") {
-            country = geocode[comp][i]["long_name"]
-        }
-    };
-
-    if (country == null) {
-        "Could not find country"
-    }
-
-    return country;
-}
-
 
 // Get info about latitude and longitude
-function requestGeocode(lat, lng) {
+function requestGeocode(lat, lng, callback, options) {
     var latlng = new google.maps.LatLng(lat, lng);
     var geocoder = new google.maps.Geocoder();
     geocoder.geocode({ 'latLng': latlng }, function (results, status) {
@@ -42,7 +15,13 @@ function requestGeocode(lat, lng) {
         if (status == google.maps.GeocoderStatus.OK) {
             first = results[0]; // Assume results have same country
 
-            return first
+            callback(first, lat, lng, options)
+        }
+
+        else {
+            // alert('Geocoder failed due to: ' + status);
+            console.log('Geocoder failed due to: ');
+            console.log(status);
         }
     });
 }
--- js/model.js
+++ js/model.js
@@ -3,24 +3,44 @@ R = 6371.0
 alpha = 0.3
 infected_blocks = []
 not_surrounded = []
+list_of_blocks = {}
 
-// Creates a new block with the proper density
-function newBlock (country, lat, lng) {
+function infectBlockCallback (geocode, lat, lng, options) {
+	var country = getCountry(geocode);
+	infectBlockCountry(lat, lng, country, options[0], options[1]);
+}
+
+function infectBlockCountry (lat, lng, country, x_index, y_index) {
 	var people_density = densities[country];
-	return {"S": people_density, "I": 0, "R": 0, "lat": lat, "lng": lng, "Neighbors": {
-		"N": null,
-		"E": null,
-		"W": null,
-		"S": null,
-		"NE": null,
-		"NW": null,
-		"SE": null,
-		"SW": null
-	}};
+	var block = {"S": people_density - 1, 
+			"I": 1, 
+			"R": 0, 
+			"lat": lat, 
+			"lng": lng, 
+			"x": x_index, 
+			"y": y_index 
+	}
+
+	if (!(x_index in list_of_blocks)) {
+		list_of_blocks[x_index] = {};
+	}
+
+	infected_blocks.push([x_index, y_index])
+	not_surrounded.push([x_index, y_index])
+	list_of_blocks[x_index][y_index] = block;
+	console.log("Block infected");
+}
+
+function infectBlock(fromBlock, dir, options) {
+	lat1 = fromBlock.lat
+	lng1 = fromBlock.lng
+	new_lat_lng = newLatLng(lat1, lng1, dir);
+	requestGeocode(new_lat_lng[0], new_lat_lng[1], infectBlockCallback, options)
 };
 
 // Runs one step of the SIR model on the block. Just returns on ocean block
-function SIR (block) {
+function SIR (coord) {
+	block = list_of_blocks[coord[0]][coord[1]]
 	if (block.ocean == true) {
 		return;
 	} 
@@ -35,43 +55,75 @@ function SIR (block) {
 	block.R += alpha * S * I / N;
 }
 
+function getCountry (geocode) {
+	// Iterate through properties of result to find country
+	var comp = "address_components"; // Key to parse out country names
+	var country = null;
+	for (var i = 0; i < geocode[comp].length; i++) {
+	    if (geocode[comp][i].types[0] == "country") {
+	        country = geocode[comp][i]["long_name"];
+	    }
+	};
+
+	if (country == null) {
+	    alert("Could not find country");
+	}
+
+	return country;
+}
+
 // Start the infection from this point
-function launchModel (country, lat, lng) {
+function launchModel (geocode, lat, lng) {
+
+	var country = getCountry(geocode)
 
 	// Click location is site of first Zombie
-	var starter_block = newBlock(country, lat, lng)
-	starter_block.S -= 1
-	starter_block.I += 1
+	var starter_block = infectBlockCountry(lat, lng, country, 0, 0)
 
-	// All the neighboring blocks are initialized as susceptible
-	starter_block.Neighbors.N = newBlock(country, lat, lng);
-	starter_block.Neighbors.E = newBlock(country, lat, lng);
-	starter_block.Neighbors.W = newBlock(country, lat, lng);
-	starter_block.Neighbors.S = newBlock(country, lat, lng);
-	starter_block.Neighbors.NE = newBlock(country, lat, lng);
-	starter_block.Neighbors.NW = newBlock(country, lat, lng);
-	starter_block.Neighbors.SE = newBlock(country, lat, lng);
-	starter_block.Neighbors.SW = newBlock(country, lat, lng);
 
-	infected_blocks.push(starter_block)
-	not_surrounded.push(starter_block)
 
-	// Run the steps of the simulation (replace with ticks)
-	window.setInterval(function() {
+	// for (var i = 0; i < 10; i++) {
+	// 	for (var j = 0; j < infected_blocks.length; j++) {
+	// 		SIR(infected_blocks[j])
+	// 	};
+	// 	spread()
+	// 	console.log(infected_blocks);
+	// };
+
+
+	// Run the steps of the simulation
+	var timesRun = 0
+	var interval = window.setInterval(function() {
 		for (var j = 0; j < infected_blocks.length; j++) {
+			console.log("infecting block");
 			SIR(infected_blocks[j])
 		};
 		spread()
-	}, 1000)
-	console.log(infected_blocks);
+
+		timesRun += 1
+		console.log("Times run: " + timesRun);
+		if (timesRun >= 100) {
+			clearInterval(interval);
+			console.log(list_of_blocks);
+			console.log(infected_blocks);
+			console.log(not_surrounded);
+
+		}
+
+	}, 5000)
 }
 
 // Find all the locations around this block that are not infected
-function vulnerable_neighbors (block) {
-	vulnerable = []
-	for (var dir in block.Neighbors) {
-		if (block.Neighbors[dir].I == 0 && block.Neighbors[dir].R == 0) {
-			vulnerable.push(dir)
+function vulnerable_neighbors (x, y) {
+	vulnerable = [];
+	list = calculate_Neighbors(x, y);
+
+	for (var dir in list) {
+		item = list[dir]
+		i = item[0]
+		j = item[1]
+		if (list_of_blocks[i] == undefined || (list_of_blocks[i][j] == null)) {
+			vulnerable.push(item)
 		}
 	}
 
@@ -83,41 +135,34 @@ function spread () {
 	console.log("spread!");
 
 	// Select number of zombies that will spread
-	spreading_zombies = Math.floor(Math.random() * not_surrounded.length) + 1;
+	// spreading_zombies = Math.floor(Math.random() * not_surrounded.length) + 1;
+	spreading_zombies = 1;
 	for (var i = 0; i < spreading_zombies; i++) {
 
 		// Select a block to spread from
-		spread_point = Math.floor(Math.random() * not_surrounded.length)
+		point_index = Math.floor(Math.random() * not_surrounded.length)
+		point = not_surrounded[point_index]
 
 		// Get a list of uninfected neighbors and select one of them randomly
-		vulnerable_list = vulnerable_neighbors(not_surrounded[spread_point])
+		vulnerable_list = vulnerable_neighbors(point[0], point[1])
 		targetIndex = Math.floor(Math.random() * vulnerable_list.length)
-		targetDir = vulnerable_list[targetIndex]
+		target = vulnerable_list[targetIndex]
 
 		// If this block is surrounded by infected, remove it. 
 		if (vulnerable_list.length == 1) {
-			not_surrounded.splice(spread_point, 1);
+			not_surrounded.splice(point_index, 1);
 		} else if (vulnerable_list.length == 0) {
-			return;
+			not_surrounded.splice(point_index, 1);
+			continue;
 		}
 
 		// Infect targeted block
-		console.log(targetDir);
-		target = not_surrounded[spread_point].Neighbors[targetDir]
-		target.S -= 1
-		target.I += 1
-		infected_blocks.push(target)
-		not_surrounded.push(target)
-		console.log(target)
+		infectBlock(list_of_blocks[point[0]][point[1]], target[2], [target[0], target[1]])
 	};
 }
 
-function infectBlock (diseased_block, new_block, dir) {
-	console.log("hi");
-}
-
 // Get the lat/long of a block in the given direction
-function newLatLng (lat1, lng1, d, brng) {
+function newLatLng (lat1, lng1, brng) {
 	var lat2 = 0
 	var lng2 = 0
 
@@ -134,4 +179,17 @@ function newLatLng (lat1, lng1, d, brng) {
 	}
 
 	return [lat1, lng2]
+}
+
+function calculate_Neighbors (x_index, y_index) {
+	return [
+		[x_index - 1, y_index + 1, "NW"],
+		[x_index, y_index + 1, "N"],
+		[x_index + 1, y_index + 1, "NE"],
+		[x_index - 1, y_index, "W"],
+		[x_index + 1, y_index, "E"],
+		[x_index - 1, y_index - 1, "SW"],
+		[x_index, y_index - 1, "S"],
+		[x_index + 1, y_index - 1, "SE"],
+	]
 }
